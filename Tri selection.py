14f2c5d8dc736dc3224a5aab883cec247a799282
nanutra <73894579+nanutra@users.noreply.github.com>
2021-01-02 11:57:59 +0100

    Create Tri selection.py

---
+++ Tri selection.py
@@ -0,0 +1,17 @@
+def tri_selection(tab):
+   for i in range(len(tab)):
+       min = i
+       for j in range(i+1, len(tab)):
+           if tab[min] > tab[j]:
+               min = j
+
+       tmp = tab[i]
+       tab[i] = tab[min]
+       tab[min] = tmp
+   return tab
+tab = [98, 22, 15, 32, 2, 74, 63, 70]
+ 
+tri_selection(tab)
+ 
+print ("Le tableau trié par Selection est:")
+print (tab)
